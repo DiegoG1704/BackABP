932f4abba14ba1837d790b618f4d3580f0d9dcf3
DiegoG1704 <dgst1704@gmail.com>
2025-03-29 18:51:00 -0500

    cambiazo

--- src/config.js
+++ src/config.js
@@ -3,14 +3,14 @@ const { config } = require("dotenv");
 
 config()
 module.exports = {
-    // PORT: 3000,
-    // DB_USER: 'garcgfzx_MasSalud',
-    // DB_HOST: 'localhost',
-    // DB_PASSWORD: 'Qe7bqWl[I2Z,',
-    // DB_DATABASE: 'garcgfzx_ABP'
     PORT: 3000,
-    DB_USER: 'root',
+    DB_USER: 'garcgfzx_MasSalud',
     DB_HOST: 'localhost',
-    DB_PASSWORD: '',
-    DB_DATABASE: 'interfazusuarios'
+    DB_PASSWORD: 'Qe7bqWl[I2Z,',
+    DB_DATABASE: 'garcgfzx_ABP'
+    // PORT: 3000,
+    // DB_USER: 'root',
+    // DB_HOST: 'localhost',
+    // DB_PASSWORD: '',
+    // DB_DATABASE: 'interfaz'
   };
